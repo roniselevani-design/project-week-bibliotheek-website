ae01fe01cbe66ee6aa7d0947eb91e8900d67ad45
kooswerkhoven <koos1610werkhoven@gmail.com>
2026-01-21 11:03:12 +0100

    Revert "html naar php en inloggen gemaakt"
    This reverts commit 7c30175a1c96e57d6e22d9766960cad8b72ae8f3.

--- Projectweek bieb/html/uitloggen.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start(); // Pak de huidige sessie op
-session_destroy(); // Vernietig alle sessie-data (uitloggen)
-header("Location: index.php"); // Stuur de gebruiker terug naar de homepagina
-exit();
-?>
